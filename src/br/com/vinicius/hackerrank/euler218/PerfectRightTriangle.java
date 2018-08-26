5ba648d2e500c0f12976579ae63a2e219afd61a9
vinicius <viniciussf@hotmail.com>
2018-08-26 14:14:15 -0300

    Primeira melhoria em performance

--- src/br/com/vinicius/hackerrank/euler218/PerfectRightTriangle.java
+++ src/br/com/vinicius/hackerrank/euler218/PerfectRightTriangle.java
@@ -3,10 +3,10 @@ package br.com.vinicius.hackerrank.euler218;
 import java.util.Scanner;
 
 class PerfectRightTriangle {
-	private int[] hypotenuses;
-	private int[][][] triangles;
+	private long[] hypotenuses;
+	private long[] totNPerf;
 
-	public PerfectRightTriangle(int[] hypotenuses) throws IllegalAccessException {
+	public PerfectRightTriangle(long[] hypotenuses) throws IllegalAccessException {
 		long max = (long) (2 * Math.pow(10, 18));
 		for (int i = 0; i < hypotenuses.length; i++) {
 			if (hypotenuses[i] < 25 || hypotenuses[i] > max) {
@@ -16,37 +16,39 @@ class PerfectRightTriangle {
 		this.hypotenuses = hypotenuses;
 	}
 
-	public long count() {
-		triangles = new int[hypotenuses.length][][];
+	public void count() {
+		totNPerf = new long[hypotenuses.length];
 
-		boolean isPerf = true;
 		boolean isAreaPerf = false;
-		long count = 0;
-		int c2 = 0, h = 0, c = 0;
+		long c2 = 0, h = 0, c = 0;
 
-		for (int i = 0; i < hypotenuses.length; i++) {
-			c = 0;
-			while (c <= hypotenuses[i]) {
-				c++;
+		hyp: for (int i = 0; i < hypotenuses.length; i++) {
+			c = hypotenuses[i];
+			while (c > 0) {
+				c--;
+				if (!isHypotenusePerfectSquare(c)) {
+					continue;
+				}
 				c2 = c * c;
 				outer: for (int a = 1; a < c; a++) {
 					for (int b = 1; b < c; b++) {
 						if ((h = a * a + b * b) > c2) {
 							break;
 						} else if (h == c2) {
-							triangles[i] = new int[2][3];
 							// Inserindo os triangulos que sao simetricos por
 							// reflexao.
+
+							if (!isPrimitiveRightTriangle(a, b, c)) {
+								continue;
+							}
+
 							isAreaPerf = isPerfectArea(a, b);
-							isPerf = isHypotenusePerfectSquare(c) && isPrimitiveRightTriangle(a, b, c);
 
 							// contando os perfeitos que nao sao super perfeitos
-							if (isPerf && !isAreaPerf) {
-								count++;
-								triangles[i][0] = new int[] { a, b, c };
-								StringBuilder s = new StringBuilder();
-								s.append("(").append(a).append(", ").append(b).append(", ").append(c).append(")");
-								System.out.println(s.toString());
+							if (!isAreaPerf) {
+								totNPerf[i]++;
+							} else {
+								break hyp;
 							}
 							break outer;
 						}
@@ -54,42 +56,31 @@ class PerfectRightTriangle {
 				}
 			}
 		}
-		return count;
 	}
 
-	public String printTriangles() {
-		StringBuilder s = new StringBuilder();
-		for (int i = 0; i < triangles.length; i++) {
-			if (triangles[i] != null && triangles[i][0] != null) {
-				s.append("(").append(triangles[i][0][0]).append(", ").append(triangles[i][0][1]).append(", ")
-						.append(triangles[i][0][2]).append(")\n");
-			}
-
-			if (triangles[i] != null && triangles[i][1] != null) {
-				s.append("(").append(triangles[i][1][0]).append(", ").append(triangles[i][1][1]).append(", ")
-						.append(triangles[i][1][2]).append(")\n");
-			}
+	public void print() {
+		for (int i = 0; i < totNPerf.length; i++) {
+			System.out.println(totNPerf[i]);
 		}
-		return s.toString();
 	}
 
-	private boolean isPrimitiveRightTriangle(int a, int b, int c) {
+	private boolean isPrimitiveRightTriangle(long a, long b, long c) {
 		return GCD(a, b) == 1 && GCD(b, c) == 1;
 	}
 
-	private boolean isPerfectArea(int a, int b) {
+	private boolean isPerfectArea(long a, long b) {
 		if (a % 2 != 0 && b % 2 != 0) {
 			return false;
 		}
-		int area = a * b / 2;
+		long area = a * b / 2;
 		return area % 6 == 0 && area % 28 == 0;
 	}
 
-	private boolean isHypotenusePerfectSquare(int c) {
+	private boolean isHypotenusePerfectSquare(long c) {
 		if (c <= 1) {
 			return true;
 		}
-		int m = c / 2;
+		long m = c / 2;
 		do {
 			if (m * m == c) {
 				return true;
@@ -100,12 +91,12 @@ class PerfectRightTriangle {
 		return false;
 	}
 
-	private int GCD(int x, int y) {
+	private long GCD(long x, long y) {
 		if (x == 1 || y == 1) {
 			return 1;
 		}
 
-		int m = x <= y ? x / 2 : y / 2;
+		long m = x <= y ? x / 2 : y / 2;
 		do {
 			if (x % m == 0 & y % m == 0) {
 				return m;
@@ -122,16 +113,17 @@ class PerfectRightTriangle {
 			return;
 		}
 
-		int[] hypotenuses = new int[q];
-		while (--q > 0) {
-			hypotenuses[q] = in.nextInt();
+		long[] hypotenuses = new long[q];
+		while (--q >= 0) {
+			hypotenuses[q] = in.nextLong();
 		}
 		in.close();
 
 		PerfectRightTriangle p;
 		try {
 			p = new PerfectRightTriangle(hypotenuses);
-			System.out.println(p.count());
+			p.count();
+			p.print();
 		} catch (IllegalAccessException e) {
 			return;
 		}
