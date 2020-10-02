771562de2438e7afe281181708cebd4eeb25fa9d
Vinicius <viniciussf@hotmail.com>
2020-10-02 06:41:42 -0300

    Estrategia de arvore par asimplificar o codigo

--- src/br/com/vinicius/hackerrank/euler201/indextable/SubsetSumIndexTable.java
+++ src/br/com/vinicius/hackerrank/euler201/indextable/SubsetSumIndexTable.java
@@ -3,19 +3,20 @@ package br.com.vinicius.hackerrank.euler201.indextable;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Scanner;
 import java.util.Map.Entry;
+import java.util.Scanner;
 import java.util.Set;
 
 class SubsetSumIndexTable {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+		final Scanner in = new Scanner(System.in);
 		String[] line = in.nextLine().split("\\s+");
 		int n = 0, m = 0;
 		try {
 			n = Integer.parseInt(line[0]);
 			m = Integer.parseInt(line[1]);
-		} catch (Exception e) {
+		}
+		catch (final Exception e) {
 			in.close();
 			return;
 		}
@@ -23,7 +24,7 @@ class SubsetSumIndexTable {
 			in.close();
 			return;
 		}
-		int[] numbers = new int[n];
+		final int[] numbers = new int[n];
 
 		line = in.nextLine().split("\\s+");
 		in.close();
@@ -34,21 +35,23 @@ class SubsetSumIndexTable {
 		for (int i = 0; i < line.length; i++) {
 			try {
 				numbers[i] = Integer.parseInt(line[i]);
-			} catch (Exception e) {
+			}
+			catch (final Exception e) {
 				return;
 			}
 		}
 		try {
 			System.out.println(new SubsetSumIndexTable(numbers, m).count());
-		} catch (Exception e) {
+		}
+		catch (final Exception e) {
 			return;
 		}
 
 	}
 
-	int m = 0;
+	private int m = 0;
 
-	Map<Integer, Boolean> map = new HashMap<>(1000);
+	private final Map<Integer, Boolean> map = new HashMap<>(1000);
 	private int[] numbers = null;
 	private Boolean ok = false;
 	private int parcSum = 0;
@@ -60,7 +63,7 @@ class SubsetSumIndexTable {
 		if (numbers.length < 1 || numbers.length > 100 || m < 1 || m > numbers.length) {
 			throw new Exception();
 		}
-		for (int i : numbers) {
+		for (final int i : numbers) {
 			if (i < 1 || i > 100) {
 				throw new IllegalArgumentException();
 			}
@@ -79,20 +82,20 @@ class SubsetSumIndexTable {
 			return totSum;
 		}
 
-		int[] idxTable = new int[m - 1];
+		final int[] idxTable = new int[m - 1];
 		for (int i = 0; i < idxTable.length; i++) {
 			idxTable[i] = i;
 		}
 
-		int[] idxMax = new int[m - 1];
+		final int[] idxMax = new int[m - 1];
 		for (int i = 0; i < idxMax.length; i++) {
 			idxMax[i] = numbers.length - m + i;
 		}
 
 		count(new int[m - 1], idxTable, idxMax, idxTable.length - 1);
 
-		Set<Entry<Integer, Boolean>> entry = map.entrySet();
-		for (Entry<Integer, Boolean> e : entry) {
+		final Set<Entry<Integer, Boolean>> entry = map.entrySet();
+		for (final Entry<Integer, Boolean> e : entry) {
 			if (!e.getValue()) {
 				totSum += e.getKey();
 			}
@@ -121,7 +124,8 @@ class SubsetSumIndexTable {
 			// System.out.println(s.toString() + numbers[i] + "}=" + sum);
 			if ((ok = map.get(sum)) == null) {
 				map.put(sum, Boolean.FALSE);
-			} else if (ok.equals(Boolean.FALSE)) {
+			}
+			else if (ok.equals(Boolean.FALSE)) {
 				map.put(sum, Boolean.TRUE);
 			}
 		}
@@ -138,7 +142,8 @@ class SubsetSumIndexTable {
 				idxTable[i] = ++idx;
 			}
 
-		} else if (col + 1 < idxTable.length && idxTable[col] == idxTable[col + 1]) {
+		}
+		else if (col + 1 < idxTable.length && idxTable[col] == idxTable[col + 1]) {
 			idxTable[col]--;
 			col++;
 			idxTable[col]++;
